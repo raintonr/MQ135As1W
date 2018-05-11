6c45a2233741d376de3d0eac3eeaadac67436c58
Robin Rainton <robin@rainton.com>
2018-05-11 19:46:57 +0200

    Simplified version

--- MQ135As1W.ino
+++ MQ135As1W.ino
@@ -31,18 +31,14 @@
 /**************************************************************************/
 
 #include <EEPROM.h>
-#include "MQ135.h"
 #include "OneWireHub.h"
 #include "OneWireItem.h"
 #include <TrueRandom.h>
 
-// Define if to use DS2438 or DS18B20, comment out not needed type, default DS2438, DS2401 mode not integrated at the moment
-//#define USE_DS18B20
+// Define if to use DS2438 (other options removed in this version)
 #define USE_DS2438
-//#define USE_DS2401
 
 // Pin definition
-#define PIN_D_MQ135    2  // Pin for digital input from MQ135
 #define PIN_A_MQ135   A0  // Pin for analog input from MQ135
 #define PIN_ONE_WIRE   8  // 1-Wire pin
 
@@ -55,51 +51,8 @@
 // If you don't find this useful, comment next line out
 #define INIT_DELAY 180000
 
-#ifdef USE_DS18B20
-  /*
-   * DS18B20 can encode max 2000 values in range 0 – 125 °C, so the max ppm would be 2000
-   * With this ratio you can extend it (for example make it to 5.0), but you will loose precision on lower values
-   * Ratio 5.0 will support ppm range 0 – 10000
-   *    Conversion => (Temp / 125.0) * 2000 * RATIO
-   * In Loxone use correction and for input value 2 use "1" and for dispay value 16 * DS18B20_RATIO (default 80)
-   */
-  #define DS18B20_RATIO              5.0F
-  #define DS18B20_MAX_VALUE       2000.0F // Do not touch
-  #define DS18B20_MAX_TEMPERATURE  125.0F // Do not touch
-#endif
-
-#ifdef USE_DS2438
-  /*
-   * DS2438 can encode 5760 values in range –55 – 125 °C
-   * SW will use all of these to get highest possible precision
-   * Temp value will be used to transfer modulo of ppm (5760)
-   * VAD value will be used to transfer divison of ppm (5760)
-   * With this conversion, max usable ppm is 5628934.50 ppm which should be enough...
-   *    Conversion => (((Temp + 55.0) / 180) + (VAD * 100)) * 5760
-   * In Loxone use Formula I1 = Temperature, I2 = VAD, Formula = (((I1 + 55.0) / 180) + (I2 * 100)) * 5760
-   * 
-   * VDD value contains analog measurement
-   * Vsens value is always 0
-   */
-  #define DS2438_MAX_VALUE          5760 // Do not touch
-  #define DS2438_MAX_VALUE_F     5760.0F // Do not touch
-  #define DS2438_MIN_TEMPERATURE  -55.0F // Do not touch
-  #define DS2438_MAX_TEMPERATURE  125.0F // Do not touch
-  #define DS2438_TEMP_RANGE       180.0F // Do not touch
-  #define DS2438_VAD_STEPS          1023 // Do not touch
-#endif
-
 // Init Hub
 OneWireHub hub = OneWireHub(PIN_ONE_WIRE);
-
-// Init MQ135 library
-MQ135 mq = MQ135(PIN_A_MQ135);
-
-// DS18B20
-#ifdef USE_DS18B20
-  #include "DS18B20.h"
-  DS18B20 *ds18b20;
-#endif
 
 // DS2438
 #ifdef USE_DS2438
@@ -121,11 +74,6 @@ void setup() {
 
     pinMode(LED_BUILTIN, OUTPUT);
 
-    // Init digital input
-    #ifdef USE_DS2401
-      pinMode(PIN_D_MQ135, INPUT);
-    #endif;
-
     // 1W address storred in EEPROM
     if (EEPROM.read(0) == '#') {
         Serial.println("Reading 1W address from EEPROM");
@@ -142,23 +90,16 @@ void setup() {
         EEPROM.write(0, '#');
     }
 
-    // Init DS18B20 1W
-    #ifdef USE_DS18B20
-      ds18b20 = new DS18B20(DS18B20::family_code, addr[1], addr[2], addr[3], addr[4], addr[5], addr[6]);
-      hub.attach(*ds18b20);
-    #endif
-
     // Init DS2438
     #ifdef USE_DS2438
       ds2438 = new DS2438New(DS2438New::family_code, addr[1], addr[2], addr[3], addr[4], addr[5], addr[6]);
       ds2438->setCurrent(0);
+      ds2438->setVDDVoltage(0);
+      ds2438->setTemperature((int8_t) 0);
       hub.attach(*ds2438);
     #endif
 
     // Log addresses
-    #ifdef USE_DS18B20
-      dumpAddress("1-Wire DS18B20 MQ135 device address: ", ds18b20, "");
-    #endif
     #ifdef USE_DS2438
       dumpAddress("1-Wire DS2438 MQ135 device address: ", ds2438, "");
     #endif
@@ -171,28 +112,21 @@ void setup() {
 // Loop call
 void loop() {
     // Read value from sensor
-    float ppm = mq.getPPM();
-    Serial.print("Reading PPM: "); Serial.println(ppm);
 
-    #ifdef USE_DS18B20
-      // Assign DS18B20 data
-      uint16_t ds1820 = (ppm < (DS18B20_MAX_VALUE * DS18B20_RATIO)) ? (uint16_t)(ppm / DS18B20_RATIO) : DS18B20_MAX_VALUE;
-      ds18b20->setTemperature((float)((ds1820 / DS18B20_MAX_VALUE) * DS18B20_MAX_TEMPERATURE));
-    #endif
+    uint16_t mq135 = analogRead(PIN_A_MQ135);
+    Serial.print("Read raw value: "); Serial.println(mq135);
 
     #ifdef USE_DS2438
-      // Assign DS2438 data
-      uint16_t ds2438temp = (uint32_t)ppm % DS2438_MAX_VALUE;
-      uint16_t ds2438vad = min((uint16_t)((uint32_t)ppm / DS2438_MAX_VALUE), 1000);
-      ds2438->setTemperature(((ds2438temp / DS2438_MAX_VALUE_F) * DS2438_TEMP_RANGE) + DS2438_MIN_TEMPERATURE);
-      ds2438->setVADVoltage(ds2438vad);
-      ds2438->setVDDVoltage(analogRead(PIN_A_MQ135));
+      ds2438->setVADVoltage(mq135);
     #endif
 
     // Polling one wire data
     unsigned long stopPolling = millis() + READING_INTERVAL;
-    while (stopPolling > millis())
+    unsigned long longFlash = millis() + 500;
+    while (stopPolling > millis()) {
+        digitalWrite(LED_BUILTIN, ((millis() < longFlash) || (millis() % 1000) < 100) ? 1 : 0);
         hub.poll();
+    }
 }
 
 // Prints the device address to console
